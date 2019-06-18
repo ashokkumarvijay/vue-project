b5132b7b1ec62151fdc29eecd1e987d94d3a628f
user_namels <rajaashokkumar907@gmail.com>
2019-06-18 16:43:41 +0530

    changes

--- src/components/portfolio/portfolio.vue
+++ src/components/portfolio/portfolio.vue
@@ -1,5 +1,37 @@
 <template>
     <div>
-        <h1>The Portfolio Component</h1>
+    <div v-for="stock in stocks">
+        <div class=" row col-sm-6 col-md-4 card card-success ">
+            <div class="card-header text-white" style="height: 40px;background-color:#42b983;width:365px;">Name:{{stock.name}},quantity:{{stock.quantity}}</div>
+            <div class="card-body">
+                <div class="float-left col-md-9" >
+                    <input type="number" placeholder="Enter Sell" :value="stock.quantity" disabled class="form-control">
+                </div>
+                <div class="float-right">
+                    <button class="btn btn-success" >Sell</button>
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
 </template>
+<script>
+import Stock from '../stocks/stock.vue'
+export default {
+  data: function () {
+    return {
+      quantity1: 0
+    }
+  },
+  computed: {
+    stocks () {
+      return this.$store.getters.Stocks
+    }
+  },
+  components: {
+    'stock': Stock
+  }
+}
+</script>
+<style>
+</style>
